1e75f317354d13235410a6787219f68cc0f43ab6
Anshu Raj <anshusopinion@gmail.com>
2021-05-18 08:06:30 +0530

    some mod

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 
 const App = () => {
-  return (
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  );
+  return <div></div>;
 };
 
 export default App;
